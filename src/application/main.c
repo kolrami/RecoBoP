6fab3352dba9a48f4c5e0d82140b404a220575d4
Christoph Rüthing <ruething@mail.upb.de>
2015-07-12 12:39:15 +0200

    Added NON WORKING fixed point transformation

--- src/application/main.c
+++ src/application/main.c
@@ -2,16 +2,44 @@
 #include "reconos_app.h"
 #include "mbox.h"
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
 
-#define MAX_ANGLE 90
-#define SLEEP 1000
+#define MAX_ANGLE 60
+#define SLEEP 20000
+
+inline double radians(double deg) {
+	return deg * (M_PI / 180.0);
+}
+
+inline uint32_t fixed(double d) {
+	int i;
+	uint32_t r;
+	double d_old = d;
+
+	if (d < 0) {
+		r = 1;
+		d = -d;
+	} else {
+		r = 0;
+	}
+
+	for (i = 0; i >= -8; i--) {
+		if (d >= pow(2, i)) {
+			r |= 1 << (1 - i);
+			d -= pow(2, i);
+		}
+	}
+
+	return r;
+}
 
 int main(int argc, char **argv) {
 	int a, i;
+	uint32_t s, c;
 	char line[128];
 	printf("Hello World\n");
 
@@ -22,7 +50,7 @@ int main(int argc, char **argv) {
 	reconos_thread_create_hwt_inverse();
 
 	for (i = 0; i < 6; i++)
-		mbox_put(inverse_cmd_ptr, 0 | 1 << 22 | 0 << 12 | 0 << 3 | i << 0);
+		mbox_put(inverse_cmd_ptr, 0 | fixed(0) << 22 | fixed(0) << 12 | 0 << 3 | i << 0);
 	sleep(1);
 
 
@@ -30,29 +58,41 @@ int main(int argc, char **argv) {
 	while(1) {
 		for (a = 0; a < MAX_ANGLE; a++) {
 			for (i = 0; i < 6; i++)
-				mbox_put(inverse_cmd_ptr, 0 | 1 << 22 | 0 << 12 | a << 3 | i << 0);
+				mbox_put(inverse_cmd_ptr, 0 | fixed(1) << 22 | fixed(0) << 12 | a << 3 | i << 0);
 			usleep(SLEEP);
 		}
 
 		for (a = MAX_ANGLE; a > 0; a--) {
 			for (i = 0; i < 6; i++)
-				mbox_put(inverse_cmd_ptr, 0 | 1 << 22 | 0 << 12 | a << 3 | i << 0);
+				mbox_put(inverse_cmd_ptr, 0 | fixed(1) << 22 | fixed(0) << 12 | a << 3 | i << 0);
 			usleep(SLEEP);
 		}
 
 		for (a = 0; a > -MAX_ANGLE; a--) {
 			for (i = 0; i < 6; i++)
-				mbox_put(inverse_cmd_ptr, 0 | -1 << 22 | 0 << 12 | -a << 3 | i << 0);
+				mbox_put(inverse_cmd_ptr, 0 | fixed(-1) << 22 | fixed(0) << 12 | -a << 3 | i << 0);
 			usleep(SLEEP);
 		}
 
 		for (a = -MAX_ANGLE; a < 0; a++) {
 			for (i = 0; i < 6; i++)
-				mbox_put(inverse_cmd_ptr, 0 | -1 << 22 | 0 << 12 | -a << 3 | i << 0);
+				mbox_put(inverse_cmd_ptr, 0 | fixed(-1) << 22 | fixed(0) << 12 | -a << 3 | i << 0);
 			usleep(SLEEP);
 		}
 	}
 
+#endif
+
+#if 0
+	while (1) {
+		for (a = 95; a < 180; a++) {
+			s = fixed(sin(radians(a)));
+			c = fixed(cos(radians(a)));
+			for (i = 0; i < 6; i++)
+				mbox_put(inverse_cmd_ptr, 0 | c << 22 | s << 12 | MAX_ANGLE << 3 | i << 0);
+			usleep(SLEEP);
+		}
+	}
 #endif
 
 #if 0
