4eb1c5e47ebce78f4f26a6047b9bed110143680b
Christoph Rüthing <ruething@mail.upb.de>
2015-08-07 20:09:40 +0200

    Adjustment of main application

--- src/application/main.c
+++ src/application/main.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
+#include <signal.h>
 
 #define MAX_ANGLE 60
 #define SLEEP 20000
@@ -19,6 +20,14 @@ inline double radians(double deg) {
 	return deg * (M_PI / 180.0);
 }
 
+static void exit_signal(int sig) {
+	reconos_cleanup();
+
+	printf("[recobop] aborted\n");
+
+	exit(0);
+}
+
 int main(int argc, char **argv) {
 	int a, i;
 	int x, y;
@@ -27,31 +36,55 @@ int main(int argc, char **argv) {
 	int pos_x, pos_y;
 	char line[128];
 	printf("Hello World\n");
+	printf("int is %d and float is %d\n", sizeof(int), sizeof(float));
 
 	reconos_init();
 	reconos_app_init();
 
-	printf("Initializing Info\n");
-	rb_info.ctrl_touch_wait = 40000000;
+	signal(SIGINT, exit_signal);
+	signal(SIGTERM, exit_signal);
+	signal(SIGABRT, exit_signal);
 
-	//reconos_thread_createi_hwt_servo((void *)&rb_info);
+	printf("Initializing Info\n");
+	rb_info.ctrl_touch_wait = 2000000 | 0x01000000;
+
+	reconos_thread_createi_hwt_performance((void *)&rb_info);
+	reconos_thread_createi_hwt_servo((void *)&rb_info);
+	reconos_thread_createi_swt_control((void *)&rb_info);
+	reconos_thread_createi_hwt_inverse((void *)&rb_info);
+	reconos_thread_createi_hwt_inverse((void *)&rb_info);
+	reconos_thread_createi_hwt_inverse((void *)&rb_info);
 	reconos_thread_createi_hwt_touch((void *)&rb_info);
 	//reconos_thread_createi_hwt_vga((void *)&rb_info);
-	//reconos_thread_createi_hwt_control((void *)&rb_info);
-	//reconos_thread_createi_hwt_inverse((void *)&rb_info);
 	//reconos_thread_createi_swt_web((void *)&rb_info);
+	reconos_thread_createi_swt_power((void *)&rb_info);
 
+#if 0
+	while (1) {
+		printf("%f\n", rb_info.perf_control / 100000.0);
+		usleep(50000);
+	}
+#endif
+
+#if 0
 	printf("Resetting platform ...\n");
 	for (i = 0; i < 6; i++)
 		mbox_put(inverse_cmd_ptr, 0 | 0 << 22 | 0 << 12 | 0 << 3 | i << 0);
 	sleep(1);
+#endif
 
-#if 1
+#if 0
 	printf("Testing touch controller ...\n");
 	while(1) {
 		pos = mbox_get(touch_pos_ptr);
 		pos_x = (pos >> 12) & 0xfff;
+		if (((pos_x >> 11) & 0x1) == 1) {
+			pos_x |= m << 12;
+		}
 		pos_y = (pos >> 0) & 0xfff;
+		if (((pos_y >> 11) & 0x1) == 1) {
+			pos_y |= m << 12;
+		}
 		printf("touch position is %d,%d\n", pos_x, pos_y);
 		mbox_get(touch_pos_ptr);
 	}
--- src/application/recobop.h
+++ src/application/recobop.h
@@ -5,9 +5,9 @@
 #include <stdint.h>
 
 struct recobop_info {
-	uint32_t saw_vin;
-	uint32_t saw_vsense;
-	uint32_t saw_power;
+	float saw_vin;
+	float saw_vsense;
+	float saw_power;
 	uint32_t saw_pos;
 
 	uint32_t ctrl_touch_wait;
@@ -17,6 +17,28 @@ struct recobop_info {
 	uint32_t perf_inverse;
 	uint32_t perf_all;
 };
+
+static inline float rbi_saw_power(struct recobop_info *rb_info) {
+	return rb_info->saw_power;
+}
+
+static inline int rbi_saw_pos_x(struct recobop_info *rb_info) {
+	int pos_x = (rb_info->saw_pos >> 12) & 0xfff;
+	if (((pos_x >> 11) & 0x1) == 1) {
+		pos_x |= 0xfffff000;
+	}
+
+	return pos_x;
+}
+
+static inline int rbi_saw_pos_y(struct recobop_info *rb_info) {
+	int pos_y = (rb_info->saw_pos >> 0) & 0xfff;
+	if (((pos_y >> 11) & 0x1) == 1) {
+		pos_y |= 0xfffff000;
+	}
+
+	return pos_y;
+}
 
 static inline uint32_t fltofi(float f, int n, int dn) {
 	int i;
