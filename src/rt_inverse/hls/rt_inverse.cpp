775e7829c5b093e8626a7533f4540e1078b21a0f
Christoph Rüthing <ruething@mail.upb.de>
2015-08-18 18:11:03 +0200

    Added data dependencie for inverse performance

--- src/rt_inverse/hls/rt_inverse.cpp
+++ src/rt_inverse/hls/rt_inverse.cpp
@@ -152,7 +152,7 @@ ap_uint<32> inverse(ap_uint<32> data) {
 #endif
 
 #ifdef __RECONOS__
-		MBOX_PUT(performance_perf, (ap_uint<8>("21", 16), ap_uint<21>(0), data(2, 0)));
+		MBOX_PUT(performance_perf, (ap_uint<8>("21", 16), v_s_aj_l_mina, ap_uint<10>(0), data(2, 0)));
 		MBOX_PUT(servo_cmd, (v_s_aj_l_mina, cmd_l, ap_uint<18>(0)));
 #else
 		return (v_s_aj_l_mina, cmd_l, ap_uint<18>(0));
